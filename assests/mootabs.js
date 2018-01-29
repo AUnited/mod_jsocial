e4d5307d233a1cfee9cad798f4844d63e30397c7
DragonZX <DragonZX@yandex.ru>
2018-01-29 23:13:06 +0300

    Init tabs

---
+++ assests/mootabs.js
@@ -0,0 +1,47 @@
+var showFunction = function() {
+    //resets all the styles before it morphs the current one
+
+    $$('.hiddenM').setStyles({
+        'display': 'none',
+        'opacity': 0,
+        'background-color': '#fff',
+        'font-size': '16px'
+    });
+
+    //here we start the morph and set the styles to morph to
+    this.start({
+        'display': 'block',
+        'opacity': 1,
+        'background-color': '#d3715c',
+        'font-size': '31px'
+    });
+}
+
+window.addEvent('domready', function() {
+    var elOneM = $('contentoneM');
+    var elTwoM = $('contenttwoM');
+    var elThreeM = $('contentthreeM');
+    var elFourM = $('contentfourM');
+    //create morph object
+
+    elOneM = new Fx.Morph(elOneM, {
+        link: 'cancel'
+    });
+
+    elTwoM = new Fx.Morph(elTwoM, {
+        link: 'cancel'
+    });
+
+    elThreeM = new Fx.Morph(elThreeM, {
+        link: 'cancel'
+    });
+
+    elFourM = new Fx.Morph(elFourM, {
+        link: 'cancel'
+    });
+
+    $('VkM').addEvent('click', showFunction.bind(elOneM));
+    $('OkM').addEvent('click', showFunction.bind(elTwoM));
+    $('FbM').addEvent('click', showFunction.bind(elThreeM));
+    $('TwM').addEvent('click', showFunction.bind(elFourM));
+});
--- mod_jsocial.php
+++ mod_jsocial.php
@@ -9,10 +9,33 @@
 
 // no direct access
 defined('_JEXEC') or die ('Restricted Access');
+require_once ('assests/functions.php');
 
 $module = JModuleHelper::getModule('mod_jsocial');
 $moduleParams = new JRegistry($module->params);
 $options['width']   =   $moduleParams->get('width', '100%');
+$options['height']   =   $moduleParams->get('height', '100%');
+$vkid   =   $moduleParams->get('vkid', '');
+$okid   =   $moduleParams->get('okid', '');
+$fbid   =   $moduleParams->get('fbid', '');
+$tid   =   $moduleParams->get('tid', '');
+$fbtext   =   $moduleParams->get('fbtext', 'Facebook');
+$tabsheight=0;
+$bodywidth  =   $options['width'];
+$bodyheight =   $options['height'] - $tabsheight;
 
+echo '
+    <script type="application/javascript">'.'assests/mootabs.js'.'</script>
+    <ul id = "tabs">';
+if($vkid)   { echo '<li id = "VkM">VK.com</li>';}
+if($okid)   { echo '<li id = "OkM">OK.ru</li>';}
+if($fbid)   { echo '<li id = "FbM">Facebook</li>';}
+if($tid)    { echo '<li id = "TwM">Twitter</li>';}
+echo' </ul>';
 
-echo 'It works!';
+#and here are our content divs
+if($vkid)   { echo '<div id = "contentoneM" class = "hiddenM">'.vkinit().vkid($vkid, $bodywidth, $bodyheight).'</div>';}
+if($okid)   { echo '<div id = "contenttwoM" class = "hiddenM">'.okid($okid, $bodywidth, $bodyheight).'</div>';}
+if($fbid)   { echo '<div id = "contentthreeM" class = "hiddenM">'.fbinit().fbid($fbid,$fbtext, $bodywidth, $bodyheight).'</div>';}
+if($tid)    { echo '<div id = "contentfourM" class = "hiddenM">'.tinit().tid($fbid, $bodywidth, $bodyheight).'</div>';}
+
