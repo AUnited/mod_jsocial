15aa167eb2c5845bc660beb04fd68993c3fa4fef
DragonZX <DragonZX@yandex.ru>
2018-01-29 22:02:14 +0300

    OK&FB Finished

--- assests/functions.php
+++ assests/functions.php
@@ -18,6 +18,10 @@ if (!$hash) {str_replace('#','',$color);}
 return $color;
 }
 
+function NumToTF($num)
+{
+if ($num == 1){return "true";} else {return "false";}
+}
 function vkinit()
 {
     return '<script type="text/javascript" src="//vk.com/js/api/openapi.js?152"></script>';
@@ -60,12 +64,44 @@ function vkid($vkid, $width, $height)
 
 }
 
-function fbid($fbid, $width)
+function fbid($fbid, $fbtext, $width, $height)
 {
-    return '<div class="fb-page" data-href="https://www.facebook.com/facebook" data-tabs="timeline" data-width="200" data-height="300" data-small-header="true" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="true"><blockquote cite="https://www.facebook.com/facebook" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/facebook">Facebook</a></blockquote></div>';
+    $module = JModuleHelper::getModule('mod_jsocial');
+    $moduleParams = new JRegistry($module->params);
+    $tabs   =   $moduleParams->get('tabs', 'timeline');
+    $hide_cover   =   $moduleParams->get('hide_cover', 0);
+    $show_facepile   =   $moduleParams->get('show_facepile', 1);
+    $hide_cta    =   $moduleParams->get('hide_cta', 0);
+    $small_header   =   $moduleParams->get('small_header', 0);
+    $adapt_container_width  =   $moduleParams->get('adapt_container_width', 0);
+    return '<div class="fb-page" data-href="'.$fbid.'" data-tabs="'.$tabs.'" data-width="'.$width.'" data-height="'.$height.'" data-hide-cta="'.numToTF($hide_cta).'" data-small-header="'.NumToTF($small_header).'" data-adapt-container-width="'.NumToTF($adapt_container_width).'" data-hide-cover="'.NumToTF($hide_cover).'" data-show-facepile="'.NumToTF($show_facepile).'"><blockquote cite="'.$fbid.'" class="fb-xfbml-parse-ignore"><a href="'.$fbid.'">'.$fbtext.'</a></blockquote></div>';
+}
+
+function okid($okid, $width, $height)
+{
+return '<div id="ok_group_widget"></div>
+<script>
+!function (d, id, did, st) {
+  var js = d.createElement("script");
+  js.src = "https://connect.ok.ru/connect.js";
+  js.onload = js.onreadystatechange = function () {
+  if (!this.readyState || this.readyState === "loaded" || this.readyState === "complete") {
+    if (!this.executed) {
+      this.executed = true;
+      setTimeout(function () {
+        OK.CONNECT.insertGroupWidget(id,did,st);
+      }, 0);
+    }
+  }};
+  d.documentElement.appendChild(js);
+}(document,"ok_group_widget","'.$okid.'",\'{"width":'.$width.',"height":'.$height.'}\');
+</script>';
+
+
 }
 
 function tid($tid, $width)
 {
-
+    $module = JModuleHelper::getModule('mod_jsocial');
+    $moduleParams = new JRegistry($module->params);
 }
