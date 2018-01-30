04ddcc6db4e1e2e919fca517b20aace787f0954d
DragonZX <DragonZX@yandex.ru>
2018-01-30 13:23:31 +0300

    Language detection

--- assests/functions.php
+++ assests/functions.php
@@ -29,14 +29,21 @@ function vkinit()
 
 function fbinit()
 {
+    $module = JModuleHelper::getModule('mod_jsocial');
+    $moduleParams = new JRegistry($module->params);
+    $detect   =   $moduleParams->get('langd', 0);
+    $SiteLang = JFactory::getLanguage();
+    $jlang = $SiteLang->getName();
+    if($detect) {$lang=$jlang;} else {$lang="en_GB";}
     return'<div id="fb-root"></div>
-<script>(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = \'https://connect.facebook.net/ru_RU/sdk.js#xfbml=1&version=v2.11\';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, \'script\', \'facebook-jssdk\'));</script>';
+        <div>'.$lang.'</div>
+        <script>(function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = \'https://connect.facebook.net/'.$jlang.'/sdk.js#xfbml=1&version=v2.11\';
+            fjs.parentNode.insertBefore(js, fjs);
+            }(document, \'script\', \'facebook-jssdk\'));</script>';
 }
 
 function tinit()
